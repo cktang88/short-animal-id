9bea7fc5ece06d6ae75636d25adecd872538471c
cktang88 <arctangent20@yahoo.com>
2020-11-08 12:32:43 -0600

    Make opts optional

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
 // types generated via "npx typescript ./index.js --declaration --allowJs --emitDeclarationOnly --outDir types"
 
 export default function getAnimalId({ digitSuffix, alphaNumericSuffix }?: {
-    digitSuffix: boolean;
-    alphaNumericSuffix: boolean;
+    digitSuffix?: boolean;
+    alphaNumericSuffix?: boolean;
 }): string;

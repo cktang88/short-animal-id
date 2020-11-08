2b3e7eee52dba9e7c0e0674dc0c37b8f6a90750a
cktang88 <arctangent20@yahoo.com>
2020-11-08 12:28:34 -0600

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+// types generated via "npx typescript ./index.js --declaration --allowJs --emitDeclarationOnly --outDir types"
+
+export default function getAnimalId({ digitSuffix, alphaNumericSuffix }?: {
+    digitSuffix: boolean;
+    alphaNumericSuffix: boolean;
+}): string;
